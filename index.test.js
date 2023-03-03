7b902ea090661fab4c223a21936a1c54dc8f03d0
Nathan Cai <nathan97cai@gmail.com>
2023-03-03 12:23:13 -0500

    added one to many and many to many association, passed all tests

--- index.test.js
+++ index.test.js
@@ -37,4 +37,77 @@ describe("Cheese board project", () => {
         expect(cheese.title).toBe("parmesan");
         expect(cheese.description).toBe("hard, gritty, fruity");
     });
+
+    test("multiple Boards can be added to a User", async () => {
+        let nathan = await User.create({
+            name: "Nathan",
+            email: "nathan@gmail.com"
+        });
+
+        let hardBoard = await Board.create({
+            type: "hard",
+            description: "hard cheeses",
+            rating: 8
+        });
+
+        let softBoard = await Board.create({
+            type: "soft",
+            description: "soft cheeses",
+            rating: 9
+        });
+
+        await nathan.addBoard([hardBoard, softBoard]);
+
+        let nathansBoards = await nathan.getBoards();
+        expect(nathansBoards.length).toBe(2);
+        expect(nathansBoards[0] instanceof Board).toBeTruthy();
+        expect(nathansBoards[1].type).toBe("soft");
+    });
+
+    test("testing many-to-many relationship between Cheese and Board", async () => {
+        let hardBoard = await Board.create({
+            type: "hard",
+            description: "hard cheeses",
+            rating: 8
+        });
+
+        let softBoard = await Board.create({
+            type: "soft",
+            description: "soft cheeses",
+            rating: 9
+        });
+
+        let parmesan = await Cheese.create({
+            title: "parmesan",
+            description: "hard, gritty, fruity"
+        });
+
+        let asiago = await Cheese.create({
+            title: "asiago",
+            description: "smooth, mild flavor"
+        });
+
+        await hardBoard.addCheeses([parmesan, asiago]);
+        await softBoard.addCheeses([parmesan, asiago]);
+        await parmesan.addBoards([hardBoard, softBoard]);
+        await asiago.addBoards([hardBoard, softBoard]);
+
+        const cheesesOnHardBoard = await hardBoard.getCheeses();
+        expect(cheesesOnHardBoard.length).toBe(2);
+        expect(cheesesOnHardBoard[0]).toHaveProperty("title", "parmesan");
+
+        const cheesesOnSoftBoard = await hardBoard.getCheeses();
+        expect(cheesesOnSoftBoard.length).toBe(2);
+        expect(cheesesOnSoftBoard[1]).toHaveProperty("title", "asiago");
+
+        const parmesanBoards = await parmesan.getBoards();
+        expect(parmesanBoards.length).toBe(2);
+        expect(parmesanBoards[1] instanceof Board).toBeTruthy();
+
+        const asiagoBoards = await parmesan.getBoards();
+        expect(asiagoBoards.length).toBe(2);
+        expect(parmesanBoards[1].rating).toBe(9);
+    });
+
+    
 });
--- models/index.js
+++ models/index.js
@@ -1,7 +1,11 @@
-const { User } = require('./User');
-const { Board } = require('./Board');
-const { Cheese } = require('./Cheese');
+const { User } = require("./User");
+const { Board } = require("./Board");
+const { Cheese } = require("./Cheese");
 
+Board.belongsTo(User);
+User.hasMany(Board);
 
+Cheese.belongsToMany(Board, { through: "cheese_boards" });
+Board.belongsToMany(Cheese, { through: "cheese_boards" });
 
 module.exports = { User, Board, Cheese };
