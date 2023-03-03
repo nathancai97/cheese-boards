e0616c5399c1036df3c750881f788983d7e17bb2
Nathan Cai <nathan97cai@gmail.com>
2023-03-03 11:54:51 -0500

    initialized cheese, board, user, installed dependencies and hooked up db

---
+++ models/User.js
@@ -0,0 +1,9 @@
+const { sequelize } = require("../db");
+const { Sequelize } = require("sequelize");
+
+const User = sequelize.define("user", {
+  name: Sequelize.STRING,
+  email: Sequelize.STRING,
+});
+
+module.exports = { User };
